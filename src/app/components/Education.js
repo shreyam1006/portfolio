bc1b075ab259a8c5f76d66ad7e34ef06bd69f2a2
shreyam1006 <shreya.maheshwari.2000@gmail.com>
2024-12-27 14:29:32 +0530

    Education section

--- src/app/components/Education.js
+++ src/app/components/Education.js
@@ -1,58 +1,72 @@
+import { Fragment, useState } from "react";
 import "../styles/education.css";
 
 export default function Education() {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
-    <div className="flex flex-col-reverse items-center justify-center lg:block">
+    <div
+      className="flex flex-col-reverse items-center justify-center lg:block overflow-hidden"
+      onClick={() => setIsClicked(!isClicked)}
+    >
       <svg
         className="w-full lg:absolute bottom-0"
         id="eUbxc2KBHAG1"
         viewBox="0 0 455 256"
         cached="false"
       >
-        <path
-          d="M88.940698,237.042395c-.645567-33.140293-9.037246-59.697494-19.647416-62.178308s-20.176425,19.877561-22.397446,52.347599c-4.17191-4.742808-8.838797-2.121739-12.218951,6.862543s-4.951754,22.944805-4.114799,36.55176c.1551,2.3714.826457,4.068421,1.60205,4.049582h66.543249c.447953.003928.876653-.567965,1.18526-1.58116s.469877-2.378245.445878-3.774062c-.181238-10.375307-1.713925-32.277954-11.397825-32.277954Z"
-          transform="translate(17.665026-18.645492)"
-          fill="#B6CDDC"
-          stroke="#000"
-        />
-        <g>
-          <rect
-            width="80"
-            height="80"
-            rx="0"
-            ry="0"
-            transform="translate(97.5 176)"
-            fill="#FFDF70"
-            stroke="#000"
-          />
-          <rect
-            width="50"
-            height="46"
-            rx="0"
-            ry="0"
-            transform="translate(111.219823 187)"
-            fill="#F480A3"
-            stroke="#000"
-          />
-          <line
-            x1="0"
-            y1="-23"
-            x2="0"
-            y2="23"
-            transform="translate(136.219823 210)"
-            fill="none"
-            stroke="#000"
-          />
-          <line
-            x1="-25"
-            y1="0"
-            x2="25"
-            y2="0"
-            transform="translate(137.5 210)"
-            fill="none"
-            stroke="#000"
-          />
-        </g>
+        {isClicked ? (
+          <g className="slide-section slide-down">
+            <>Damn</>
+          </g>
+        ) : (
+          <g className="slide-section">
+            <path
+              d="M88.940698,237.042395c-.645567-33.140293-9.037246-59.697494-19.647416-62.178308s-20.176425,19.877561-22.397446,52.347599c-4.17191-4.742808-8.838797-2.121739-12.218951,6.862543s-4.951754,22.944805-4.114799,36.55176c.1551,2.3714.826457,4.068421,1.60205,4.049582h66.543249c.447953.003928.876653-.567965,1.18526-1.58116s.469877-2.378245.445878-3.774062c-.181238-10.375307-1.713925-32.277954-11.397825-32.277954Z"
+              transform="translate(17.665026-18.645492)"
+              fill="#B6CDDC"
+              stroke="#000"
+            />
+            <g>
+              <rect
+                width="80"
+                height="80"
+                rx="0"
+                ry="0"
+                transform="translate(97.5 176)"
+                fill="#FFDF70"
+                stroke="#000"
+              />
+              <rect
+                width="50"
+                height="46"
+                rx="0"
+                ry="0"
+                transform="translate(111.219823 187)"
+                fill="#F480A3"
+                stroke="#000"
+              />
+              <line
+                x1="0"
+                y1="-23"
+                x2="0"
+                y2="23"
+                transform="translate(136.219823 210)"
+                fill="none"
+                stroke="#000"
+              />
+              <line
+                x1="-25"
+                y1="0"
+                x2="25"
+                y2="0"
+                transform="translate(137.5 210)"
+                fill="none"
+                stroke="#000"
+              />
+            </g>
+          </g>
+        )}
         <ellipse
           rx="17.5"
           ry="17.5"
@@ -60,6 +74,7 @@ export default function Education() {
           fill="#FFDF70"
           stroke="#000"
         />
+
         <g transform="translate(-442.012952-75.039706)">
           <g id="eUbxc2KBHAG10_to" transform="translate(562.451659,9.317477)">
             <path
@@ -96,141 +111,152 @@ export default function Education() {
             />
           </g>
         </g>
-        <rect
-          width="100"
-          height="120"
-          rx="0"
-          ry="0"
-          transform="translate(177.5 136)"
-          fill="#fff"
-          stroke="#000"
-        />
-        <rect
-          width="120"
-          height="80"
-          rx="0"
-          ry="0"
-          transform="translate(277.5 176)"
-          fill="#C8D98E"
-          stroke="#000"
-        />
-        <circle
-          r="21.25"
-          transform="translate(227.5 169.75)"
-          fill="#B6CDDC"
-          stroke="#000"
-        />
-        <g id="eUbxc2KBHAG18_ts" transform="translate(227.5,169.75) scale(1,1)">
-          <path
-            d="M48.5,38.5c-8.72381,3.466667-11.533333,6.27619-15,15-3.466667-8.72381-6.27619-11.533333-15-15c8.72381-3.466667,11.533333-6.27619,15-15c3.466667,8.72381,6.27619,11.533333,15,15Z"
-            transform="translate(-33.5,-38.5)"
-            fill="#FFDF70"
-            stroke="#000"
-          />
-        </g>
-        <rect
-          width="30"
-          height="46"
-          rx="0"
-          ry="0"
-          transform="translate(212.5 210)"
-          fill="#C6B1EA"
-          stroke="#000"
-        />
-        <circle
-          r="4.454218"
-          transform="translate(227.5 220.454218)"
-          fill="#FFDF70"
-          stroke="#000"
-        />
-        <g>
-          <rect
-            width="20"
-            height="20"
-            rx="0"
-            ry="0"
-            transform="translate(285.41936 184.75)"
-            fill="#FFDF70"
-            stroke="#000"
-          />
-          <rect
-            width="20"
-            height="20"
-            rx="0"
-            ry="0"
-            transform="translate(341.215767 185.265289)"
-            fill="#FFDF70"
-            stroke="#000"
-          />
-          <rect
-            width="20"
-            height="20"
-            rx="0"
-            ry="0"
-            transform="translate(369.58064 184.75)"
-            fill="#FFDF70"
-            stroke="#000"
-          />
-          <rect
-            width="20"
-            height="20"
-            rx="0"
-            ry="0"
-            transform="translate(369.58064 213)"
-            fill="#FFDF70"
-            stroke="#000"
-          />
-          <rect
-            width="20"
-            height="20"
-            rx="0"
-            ry="0"
-            transform="translate(341.731056 213)"
-            fill="#FFDF70"
-            stroke="#000"
-          />
-          <rect
-            width="20"
-            height="20"
-            rx="0"
-            ry="0"
-            transform="translate(313.413186 185.265289)"
-            fill="#FFDF70"
-            stroke="#000"
-          />
-          <rect
-            width="20"
-            height="20"
-            rx="0"
-            ry="0"
-            transform="translate(285.41936 213)"
-            fill="#FFDF70"
-            stroke="#000"
-          />
-          <rect
-            width="20"
-            height="20"
-            rx="0"
-            ry="0"
-            transform="translate(313.928475 213)"
-            fill="#FFDF70"
-            stroke="#000"
-          />
-        </g>
-        <g transform="translate(367.912086 10.713658)">
-          <path
-            d="M88.940698,194.665284c-.645567-10.621595-9.037246-19.133283-19.647416-19.928394s-20.176425,6.370837-22.397446,16.777613c-4.17191-1.520089-8.838797-.680026-12.218951,2.199472s-4.951754,7.353901-4.114799,11.714984c.1551.760043.826457,1.303945,1.60205,1.297907h66.543249c.447953.001259.876653-.182035,1.18526-.506768s.469877-.762237.445878-1.209602c-.181238-3.325327-1.713925-10.345212-11.397825-10.345212Z"
-            transform="translate(-36.006452 39.51805)"
-            fill="#B6CDDC"
-            stroke="#000"
-          />
-        </g>
-        <path
-          d="M34,20.6c16.568543,0,30,29.101491,30,65s-13.431457,65-30,65Z"
-          transform="matrix(0-1 1 0 141.9 170)"
-          fill="#C6B1EA"
-          stroke="#000"
-        />
+        {isClicked ? (
+          <g className="slide-section slide-down">
+            <>Wow</>
+          </g>
+        ) : (
+          <g className="slide-section">
+            <rect
+              width="100"
+              height="120"
+              rx="0"
+              ry="0"
+              transform="translate(177.5 136)"
+              fill="#fff"
+              stroke="#000"
+            />
+            <rect
+              width="120"
+              height="80"
+              rx="0"
+              ry="0"
+              transform="translate(277.5 176)"
+              fill="#C8D98E"
+              stroke="#000"
+            />
+            <circle
+              r="21.25"
+              transform="translate(227.5 169.75)"
+              fill="#B6CDDC"
+              stroke="#000"
+            />
+            <g
+              id="eUbxc2KBHAG18_ts"
+              transform="translate(227.5,169.75) scale(1,1)"
+            >
+              <path
+                d="M48.5,38.5c-8.72381,3.466667-11.533333,6.27619-15,15-3.466667-8.72381-6.27619-11.533333-15-15c8.72381-3.466667,11.533333-6.27619,15-15c3.466667,8.72381,6.27619,11.533333,15,15Z"
+                transform="translate(-33.5,-38.5)"
+                fill="#FFDF70"
+                stroke="#000"
+              />
+            </g>
+            <rect
+              width="30"
+              height="46"
+              rx="0"
+              ry="0"
+              transform="translate(212.5 210)"
+              fill="#C6B1EA"
+              stroke="#000"
+            />
+            <circle
+              r="4.454218"
+              transform="translate(227.5 220.454218)"
+              fill="#FFDF70"
+              stroke="#000"
+            />
+            <g>
+              <rect
+                width="20"
+                height="20"
+                rx="0"
+                ry="0"
+                transform="translate(285.41936 184.75)"
+                fill="#FFDF70"
+                stroke="#000"
+              />
+              <rect
+                width="20"
+                height="20"
+                rx="0"
+                ry="0"
+                transform="translate(341.215767 185.265289)"
+                fill="#FFDF70"
+                stroke="#000"
+              />
+              <rect
+                width="20"
+                height="20"
+                rx="0"
+                ry="0"
+                transform="translate(369.58064 184.75)"
+                fill="#FFDF70"
+                stroke="#000"
+              />
+              <rect
+                width="20"
+                height="20"
+                rx="0"
+                ry="0"
+                transform="translate(369.58064 213)"
+                fill="#FFDF70"
+                stroke="#000"
+              />
+              <rect
+                width="20"
+                height="20"
+                rx="0"
+                ry="0"
+                transform="translate(341.731056 213)"
+                fill="#FFDF70"
+                stroke="#000"
+              />
+              <rect
+                width="20"
+                height="20"
+                rx="0"
+                ry="0"
+                transform="translate(313.413186 185.265289)"
+                fill="#FFDF70"
+                stroke="#000"
+              />
+              <rect
+                width="20"
+                height="20"
+                rx="0"
+                ry="0"
+                transform="translate(285.41936 213)"
+                fill="#FFDF70"
+                stroke="#000"
+              />
+              <rect
+                width="20"
+                height="20"
+                rx="0"
+                ry="0"
+                transform="translate(313.928475 213)"
+                fill="#FFDF70"
+                stroke="#000"
+              />
+            </g>
+            <g transform="translate(367.912086 10.713658)">
+              <path
+                d="M88.940698,194.665284c-.645567-10.621595-9.037246-19.133283-19.647416-19.928394s-20.176425,6.370837-22.397446,16.777613c-4.17191-1.520089-8.838797-.680026-12.218951,2.199472s-4.951754,7.353901-4.114799,11.714984c.1551.760043.826457,1.303945,1.60205,1.297907h66.543249c.447953.001259.876653-.182035,1.18526-.506768s.469877-.762237.445878-1.209602c-.181238-3.325327-1.713925-10.345212-11.397825-10.345212Z"
+                transform="translate(-36.006452 39.51805)"
+                fill="#B6CDDC"
+                stroke="#000"
+              />
+            </g>
+            <path
+              d="M34,20.6c16.568543,0,30,29.101491,30,65s-13.431457,65-30,65Z"
+              transform="matrix(0-1 1 0 141.9 170)"
+              fill="#C6B1EA"
+              stroke="#000"
+            />
+          </g>
+        )}
       </svg>
       <div className="cursor-pointer hover:scale-105 transition-all bg-yellow w-fit px-5 py-2 m-3 border border-black lg:absolute">
         <h2 className="font-neutral-900 text-3xl font-bold text-pink">
