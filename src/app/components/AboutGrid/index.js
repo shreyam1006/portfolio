8f3b9a5d02138ef997ff7b3c1fb4d10dba5cc905
shreyam1006 <shreya.maheshwari.2000@gmail.com>
2025-01-08 18:31:22 +0530

    Updates AboutGrid component with personal bio and social links
    Reworks AboutGrid UI for improved aesthetics and user engagement
    Adds personal biography and introduces social media linkage

--- src/app/components/AboutGrid/index.js
+++ src/app/components/AboutGrid/index.js
@@ -3,6 +3,13 @@ import Image from "next/image";
 import ME from "../Images/ME2.png";
 import { useTrail, animated, easings } from "@react-spring/web";
 import { GRIDS } from "../../constants";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const AboutGrid = ({ setCurrentGrid, animatedStyles }) => {
   const trails = useTrail(7, {
@@ -46,27 +53,62 @@ const AboutGrid = ({ setCurrentGrid, animatedStyles }) => {
           <animated.div style={trails[0]} className="h-full w-full">
             <span className="absolute inset-0 w-full h-full transition duration-400 ease-out transform translate-x-3 translate-y-3 bg-black rounded-xl"></span>
             <span className="absolute inset-0 w-full h-full bg-skyblue border border-neutral-900 rounded-xl"></span>
-            <div className="relative p-8">
-              <div className="max-w-2xl">
-                <h1 className="text-8xl font-bold my-4">
-                  See whats in
-                  <br />
-                  it for you
-                </h1>
-                <p className="text-black mb-8">
-                  Since 2005, we have been giving advice to people around
+            <div className="relative px-12 py-10">
+              <div className="border-2 border-black border-dotted rounded-lg bg-pink w-fit px-4 py-1 font-semibold text-2xl text-black">
+                About
+              </div>
+              <div className="">
+                <p className="text-black text-4xl mt-3">Hi 👋🏻, I am</p>
+                <h2 className="text-7xl font-bold my-4 max-w-2xl">
+                  Shreya Maheshwari
+                </h2>
+                <p className="text-black text-4xl mb-4 font-medium">
+                  Software Development Engineer
                 </p>
-                <div className="flex gap-4">
-                  <button className="bg-black text-white px-6 py-3 rounded-full flex items-center gap-2 border border-black hover:scale-105 transition-transform duration-300">
-                    Discover Now
-                    <span className="inline-block w-3 h-3 bg-yellow-400 rounded-full border border-black"></span>
-                  </button>
-                  <button className="flex items-center gap-2 px-6 py-3 border border-black rounded-full hover:scale-105 transition-transform duration-300 my-4">
-                    <div className="w-10 h-10 rounded-full border border-black flex items-center justify-center">
-                      ▶
-                    </div>
-                    Watch Video
-                  </button>
+                <p className="text-black text-xl mb-8">
+                  A software developer who loves turning ideas to exciting web
+                  applications.
+                  <br />
+                  With 3+ years of experience, I specialize in creating digital
+                  experiences that feels like touching grass.
+                </p>
+                <div className="flex gap-4 mt-4">
+                  <a
+                    href="https://www.linkedin.com/in/shreyam1006"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-12 h-12 bg-yellow flex items-center justify-center text-xl transition-all hover:scale-110 cursor-pointer border border-black rounded-lg"
+                  >
+                    <FontAwesomeIcon
+                      icon={faLinkedinIn}
+                      className="text-black"
+                    />
+                  </a>
+                  <a
+                    href="https://github.com/shreyam1006"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-12 h-12 bg-sage flex items-center justify-center text-xl transition-all hover:scale-110 cursor-pointer border border-black rounded-lg"
+                  >
+                    <FontAwesomeIcon icon={faGithub} className="text-black" />
+                  </a>
+                  <a
+                    href="https://www.instagram.com/shreyaamaheshwary/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-12 h-12 bg-pink flex items-center justify-center text-xl transition-all hover:scale-110 cursor-pointer border border-black rounded-lg"
+                  >
+                    <FontAwesomeIcon
+                      icon={faInstagram}
+                      className="text-black"
+                    />
+                  </a>
+                  <a
+                    href="mailto:shreya.maheshwari.2000@gmail.com"
+                    className="w-12 h-12 bg-peach flex items-center justify-center text-xl transition-all hover:scale-110 cursor-pointer border border-black rounded-lg"
+                  >
+                    <FontAwesomeIcon icon={faEnvelope} className="text-black" />
+                  </a>
                 </div>
               </div>
             </div>
