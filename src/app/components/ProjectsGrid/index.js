5d967cf17151acae7559eb33a2121b2ce7550f23
shreyam1006 <shreya.maheshwari.2000@gmail.com>
2025-01-10 02:46:50 +0530

    Reordered

--- src/app/components/ProjectsGrid/index.js
+++ src/app/components/ProjectsGrid/index.js
@@ -62,7 +62,7 @@ export default function ProjectsGrid({ setCurrentGrid, animatedStyles }) {
       <animated.div
         style={animatedStyles}
         onClick={() => setCurrentGrid(GRIDS[0])}
-        className="lg:col-span-3 lg:row-span-3  min-h-[250px]"
+        className="order-first lg:order-none lg:col-span-3 lg:row-span-3  min-h-[250px]"
       >
         <animated.div
           style={trails[0]}
